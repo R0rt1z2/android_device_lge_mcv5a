555eda23edc163092c5eeb86c1ec1409285e6cbc
R0rt1z2 <rogerortizleal@gmail.com>
2021-11-19 22:57:21 +0100

    mcv5a: import the boot control HAL
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- device.mk
+++ device.mk
@@ -21,3 +21,12 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_engine_client
+
+# A/B Boot Control HAL
+PRODUCT_PACKAGES += \
+    bootctrl.mt6755 \
+    bootctl
+
+# A/B Static Boot Control HAL
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.mt6755
